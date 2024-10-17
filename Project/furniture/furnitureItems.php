20228a9086d142facfc31f5de076fe50ae3c8081
Fazilfareed <fazilfrd9@gmail.com>
2024-10-17 17:36:58 +0530

    updated dbs for furniture and add some new functions to furniture

--- Project/furniture/furnitureItems.php
+++ Project/furniture/furnitureItems.php
@@ -1,15 +1,16 @@
 <?php
-    include("../config/connection.php");
-    session_start();
+include("../config/connection.php");
+session_start();
 
-    if(!isset($_SESSION['uname'])){
-        header("location: login.php");
-        exit();
-    }
+if (!isset($_SESSION['uname'])) {
+    header("location: login.php");
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,56 +23,77 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
 
 </head>
+
 <body>
-    <?php include("../header/header.php");?> <hr>
+    <?php include("../header/header.php"); ?>
+    <hr>
     <div class="main-container">
-       
-<?php
-    if(isset($_GET['search'])){
-        $id = $_GET['id'];
-        $query = "SELECT * FROM f_items WHERE invoice_id = $id";
-        $result = mysqli_query($con,$query);
 
-        if(!$result){
-            die("Database query failed.");
-        }
-        
-        if(mysqli_num_rows($result) > 0){
-            
-?>
+        <?php
+        if (isset($_GET['search'])) {
+            $id = $_GET['id'];
+            $query = "SELECT * FROM f_items WHERE invoice_id = $id";
+            $result = mysqli_query($con, $query);
 
-<div class="table-con">
-<table style="width: 100%;">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>location</th>
-                <th>working</th>
-                <th>Action</th>
-            </tr>
-        </thead>
+            if (!$result) {
+                die("Database query failed.");
+            }
 
-        <tbody>
-            <?php
-                while($row = mysqli_fetch_array($result)) {
-            ?>
-                <tr>
-                    <td><?php echo $row['f_set_id']?></a></td>
-                    <td><?php echo $row['location']?></td>
-                    <td><?php echo $row['working']?></td>
-                    <td>
-                        <a href="updateItems.php?id=<?php echo $id?>&setid=<?php echo $row['f_set_id'];?>" >Edit</a>
-                    </td>
-                </tr>
-            <?php
-                }
-        }
-    }
-?>
-        </tbody>
-    </table>
-</div>
-    
+            if (mysqli_num_rows($result) > 0) {
+
+        ?>
+                <div class="table-con" style="background-color:white;margin-bottom:10px;padding:2px;">
+                    <?php
+                    $query1 = "SELECT 	f_folio_number FROM f_invoice WHERE invoice_id = $id";
+                    $result1 = $con->query($query1);
+
+                    if ($result1) {
+                        // Fetch the single row from the result
+                        $row = $result1->fetch_assoc();
+
+                        // Access the specific column
+                        $value = $row['f_folio_number'];
+
+                        // Output the value
+                        echo "<b>Folio Numbers :</b> $value";
+                    } else {
+                        echo "Query error: " . $con->error;
+                    }
+                    ?>
+                </div>
+                <div class="table-con">
+                    <table style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>location</th>
+                                <th>working</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <?php
+                            while ($row = mysqli_fetch_array($result)) {
+                            ?>
+                                <tr>
+                                    <td><?php echo $row['f_set_id'] ?></a></td>
+                                    <td><?php echo $row['location'] ?></td>
+                                    <td><?php echo $row['working'] ?></td>
+                                    <td>
+                                        <a href="updateItems.php?id=<?php echo $id ?>&setid=<?php echo $row['f_set_id']; ?>">Edit</a>
+                                    </td>
+                                </tr>
+                    <?php
+                            }
+                        }
+                    }
+                    ?>
+                        </tbody>
+                    </table>
+                </div>
+
     </div>
 </body>
+
 </html>
