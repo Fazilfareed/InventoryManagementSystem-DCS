e86fe95dc670bcbe71d85ea68e64cf86a3bc6eef
Malintha Suraj Siriwardhana <malinthassiriwardhana@gmail.com>
2024-11-03 20:07:18 +0530

    Updated CSS structure

--- Project/lab/updateItems.php
+++ Project/lab/updateItems.php
@@ -33,9 +33,18 @@ if (isset($_GET['id']) && isset($_GET['setid']) && isset($_GET['item'])) {
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/adddata.css">
     <style>
+        body{
+            background: #ececec;
+            padding-top: 50px;
+            padding-bottom: 20px;
+        }
         form {
             margin: auto;
-            width: 600px;
+            width: 800px;
+            box-shadow: 0 5px 12px 0 rgba(0, 0, 0, 0.5);
+            background-color: #d4d4d4;
+            padding: 30px;
+            padding-bottom: 50px;
         }
 
         h2 {
@@ -47,7 +56,7 @@ if (isset($_GET['id']) && isset($_GET['setid']) && isset($_GET['item'])) {
 
 <body>
     <?php include("../header/header.php"); ?>
-    <h2>Update Items</h2>
+    <h2>Update Lab Items</h2>
 
     <form action="actionItem.php" method="POST">
 
@@ -55,38 +64,38 @@ if (isset($_GET['id']) && isset($_GET['setid']) && isset($_GET['item'])) {
                                                     echo (int)$_GET['id'];
                                                 } ?> " /> <br>
 
-        <label for="setid">Set Id</label> <br>
+        <label for="setid">Set Id</label>
         <input type="text" name="setid" value="<?php if (isset($_GET['setid']) && isset($_GET['id']) && isset($_GET['item'])) {
                                                     echo $setid;
                                                 } ?>" required /> <br>
 
-        <label for="category">Category</label> <br>
+        <label for="category">Category</label>
         <input type="text" name="category" value="<?php if (isset($_GET['setid']) && isset($_GET['id']) && isset($_GET['item'])) {
                                                         echo $row1['category'];
                                                     } ?>" /><br>
 
-        <label for="item">Item</label> <br>
+        <label for="item">Item</label> 
         <input type="text" name="item" value="<?php if (isset($_GET['setid']) && isset($_GET['id']) && isset($_GET['item'])) {
                                                     echo $row1['item'];
                                                 } ?>" /><br>
 
-        <label for="serial_number">Serial number</label> <br>
+        <label for="serial_number">Serial number</label>
         <input type="text" name="serial_number" value="<?php if (isset($_GET['setid']) && isset($_GET['id']) && isset($_GET['item'])) {
                                                             echo $row1['serial_number'];
                                                         } ?>" /><br>
         <?php if ($row1['category'] == 'Laptop') { ?>
-            <label for="model_number">Model number</label> <br>
+            <label for="model_number">Model number</label>
             <input type="text" name="model_number" value="<?php if (isset($_GET['setid']) && isset($_GET['id']) && isset($_GET['item'])) {
                                                                 echo $row1['model_number'];
                                                             } ?>" /><br>
         <?php } ?>
 
-        <label for="location">Location</label> <br>
+        <label for="location">Location</label> 
         <input type="text" name="location" value="<?php if (isset($_GET['setid']) && isset($_GET['id']) && isset($_GET['item'])) {
                                                         echo $row1['location'];
                                                     } ?>" required /><br>
 
-        <label for="working">Working</label> <br>
+        <label for="working">Working</label> 
 
         <div class="radio-group">
             <div><input type="radio" name="working" value="Yes" <?php if (isset($row1['working']) && $row1['working'] == 'yes') echo 'checked'; ?> /> Yes</div>
@@ -96,8 +105,8 @@ if (isset($_GET['id']) && isset($_GET['setid']) && isset($_GET['item'])) {
             <div><input type="radio" name="working" value="D" <?php if (isset($row1['working']) && $row1['working'] == 'D') echo 'checked'; ?> /> D</div>
             <div><input type="radio" name="working" value="T" <?php if (isset($row1['working']) && $row1['working'] == 'T') echo 'checked'; ?> /> T</div>
         </div>
-        <br><br>
-        <input class="button" type="submit" name="setupdate" value="Update" style="width: 75%;" /> <br>
+        <br>
+        <input class="button" type="submit" name="setupdate" value="Update" style="width: 25%;" /> <br>
     </form>
 </body>
 
